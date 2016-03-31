a4b4faae1310aee4f88855cb7455a3a99ebd8567
aspasia <aspasia.beneti@gmail.com>
2016-03-31 11:33:28 +0200

    first commit of minimum emacs configuration

---
+++ init.el
@@ -0,0 +1,71 @@
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+  (package-initialize))
+
+;; only use use-package.el at compile-time
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)                ;; if you use :diminish
+(require 'bind-key)                ;; if you use any :bind variant
+
+
+;; Color themes
+(add-to-list 'custom-theme-load-path "~/.emacs.d/color-themes")
+(load-theme 'noctilux t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; Generic config
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Uncomment the next line if you'd like to stop Emacs from
+;; automatically creating and leaving "foo~" backup files
+;; all over the place.
+(setq make-backup-files nil)
+
+;; Spaces only (no tab characters at all)!
+(setq-default indent-tabs-mode nil)
+
+;; Always show column numbers.
+(setq-default column-number-mode t)
+
+;; Display full pathname for files.
+(add-hook 'find-file-hooks
+          '(lambda ()
+             (setq mode-line-buffer-identification 'buffer-file-truename)))
+
+;; For easy window scrolling up and down.
+(global-set-key "\M-n" 'scroll-up-line)
+(global-set-key "\M-p" 'scroll-down-line)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; Use packages
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package recentf
+  :config
+  (recentf-mode)
+  (setq recentf-exclude '(".ido.last")
+        recentf-max-saved-items 1000)
+  :bind (("C-x f" . recentf-open-files)))
+
+(use-package clojure-mode
+  :ensure t
+  :mode (("\\.clj\\'" . clojure-mode)
+         ("\\.edn\\'" . clojure-mode))
+  :init
+  (add-hook 'clojure-mode-hook #'yas-minor-mode)         
+  (add-hook 'clojure-mode-hook #'linum-mode)             
+  (add-hook 'clojure-mode-hook #'subword-mode)           
+  (add-hook 'clojure-mode-hook #'smartparens-mode)       
+  (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+  (add-hook 'clojure-mode-hook #'eldoc-mode)             
+  (add-hook 'clojure-mode-hook #'idle-highlight-mode))
+
+(use-package paredit
+  :diminish paredit-mode  
+  :ensure t)
