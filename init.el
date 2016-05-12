64c82db0ac0569d0072dfbb0d37daf7ce371ec78
aspasia <aspasia.beneti@gmail.com>
2016-05-12 15:08:43 +0200

    With TAB autocomplete for clojure and removed unused packages. - Still have to manually install paredit!

--- init.el
+++ init.el
@@ -1,16 +1,23 @@
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list
-   'package-archives
-   '("melpa" . "http://melpa.org/packages/")
-   t)
-  (package-initialize))
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+(package-initialize)
+
+;; make sure we can use packages
+(or (package-installed-p 'use-package)
+    (package-install 'use-package))
 
 ;; only use use-package.el at compile-time
 (eval-when-compile
   (require 'use-package))
 (require 'diminish)                ;; if you use :diminish
 (require 'bind-key)                ;; if you use any :bind variant
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Color themes
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/color-themes")
+(load-theme 'noctilux t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  Functions
 
@@ -29,13 +36,8 @@
  (write-region (point-min) (point-max) new-filename)
  (find-file-noselect new-filename))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Color themes
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/color-themes")
-(load-theme 'noctilux t)
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;; Generic config
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Uncomment the next line if you'd like to stop Emacs from
@@ -67,7 +69,7 @@
 (global-set-key "\C-x\C-b" 'buffer-menu)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;; Use packages
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -96,20 +98,13 @@
   :mode (("\\.clj\\'" . clojure-mode)
          ("\\.edn\\'" . clojure-mode))
   :init
-  (add-hook 'clojure-mode-hook #'yas-minor-mode)         
-  (add-hook 'clojure-mode-hook #'linum-mode)             
-  (add-hook 'clojure-mode-hook #'subword-mode)           
-  (add-hook 'clojure-mode-hook #'smartparens-mode)       
+  (add-hook 'clojure-mode-hook #'yas-minor-mode)
+  (add-hook 'clojure-mode-hook #'linum-mode)
+  (add-hook 'clojure-mode-hook #'subword-mode)
+  (add-hook 'clojure-mode-hook #'smartparens-mode)
   (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
-  (add-hook 'clojure-mode-hook #'eldoc-mode)             
+  (add-hook 'clojure-mode-hook #'eldoc-mode)
   (add-hook 'clojure-mode-hook #'idle-highlight-mode))
-
-(use-package paredit
-  :init
-  (progn
-    (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
-    (add-hook 'clojure-mode-hook 'paredit-mode)
-    (add-hook 'cider-repl-mode-hook 'paredit-mode)))
 
 (use-package cider
   :ensure cider
@@ -117,27 +112,17 @@
   (setq cider-words-of-inspiration '("NREPL is ready!!"))
   (progn
     (add-hook 'cider-mode-hook 'my/setup-cider)
-    (add-hook 'cider-repl-mode-hook 'my/setup-cider)))
+    (add-hook 'cider-repl-mode-hook 'my/setup-cider))
+  :bind (("TAB" . complete-symbol)))
 
 (use-package winner                     ; Undo and redo window configurations
   :init (winner-mode)
   :bind (("C-c b" . winner-undo)
          ("C-c f" . winner-redo)))
 
-;; Autocomplete
-(use-package ido
-  :init (progn (ido-mode 1)
-               (ido-everywhere 1))
-  :config
-  (icomplete-mode +1)
+(use-package paredit
+  :init
   (progn
-    (setq ido-case-fold t)
-    (setq ido-everywhere t)
-    (setq ido-enable-prefix nil)
-    (setq ido-enable-flex-matching t)
-    (setq ido-create-new-buffer 'always)
-    (setq ido-max-prospects 10)
-    (setq ido-use-faces nil)
-    (setq ido-default-file-method 'selected-window)
-    ;(setq ido-file-extensions-order '(".rb" ".el" ".coffee" ".js"))
-    (add-to-list 'ido-ignore-files "\\.DS_Store")))
+    (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+    (add-hook 'clojure-mode-hook 'paredit-mode)
+    (add-hook 'cider-repl-mode-hook 'paredit-mode)))
