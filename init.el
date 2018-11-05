71b980a3c57ff6d607775cf899ed87ae88b2257a
Aspasia Beneti <aspasia.beneti@gmail.com>
2018-11-05 10:19:21 +0100

    Markdown

--- init.el
+++ init.el
@@ -259,3 +259,26 @@
   :ensure t
   :after python
   :config (elpy-enable))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (markdown-mode use-package rainbow-delimiters paredit magit helm-ag exec-path-from-shell elpy cider auto-highlight-symbol))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; MARKDOWN
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
